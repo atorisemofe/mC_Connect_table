05d5a5c616962587292fe3db270ba3fd42e40204
Oreoluwa Akeredolu <oakeredolu@starmicronics.com>
2025-01-17 13:53:09 -0500

    add notification method to NotificationHub

--- mC_Connect_table/Hubs/NotificationHub.cs
+++ mC_Connect_table/Hubs/NotificationHub.cs
@@ -4,5 +4,16 @@ namespace mC_Connect_table.Hubs
 {
     public class NotificationHub : Hub
     {
+        public async Task SendNotification(NotificationMessage message)
+    {
+        // Broadcast the message to all connected clients
+        await Clients.All.SendAsync("ReceiveNotification", message);
+    }
+    }
+    public class NotificationMessage
+    {
+        public string Id { get; set; }
+        // public int TableId { get; set; }
+        public string Action { get; set; }  // e.g., "order_ready", "help_requested", etc.
     }
 }
